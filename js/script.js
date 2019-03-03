70d7018bdbe743c99febfdf9a64b79c5fb74323e
anjoltuladhar <megaanjol@gmail.com>
2019-03-03 12:21:02 +0545

    First Major Commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($){
+
+	$('.my-profile-navigation li').on('click',function(){
+		$('.my-profile-navigation li').removeClass('active');
+		$(this).addClass('active');
+
+		if ($(this).hasClass('about-nav')) {
+			aio_fe_switch_tabs(0);
+		}
+		else if ($(this).hasClass('skill-nav')) {
+			aio_fe_switch_tabs(1);
+		}
+		else if ($(this).hasClass('contact-nav')) {
+			aio_fe_switch_tabs(2);
+		}
+	});
+	$('.about-nav').trigger('click');
+	
+});
+
+function aio_fe_switch_tabs(tab_no,$=jQuery){
+	var target = $('.my-profile-working-section');
+	target.css('right', (tab_no*$('.my-profile-section-wrap').width()) + 'px');
+}
